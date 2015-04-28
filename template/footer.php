7553faf854415e09a8813523937602748d02d54d
rishwan <a.rishwan@gmail.com>
2015-04-28 23:40:45 +0500

    Template files: index, city view, person view.

---
+++ template/footer.php
@@ -0,0 +1,21 @@
+
+  <div class="container-fluid">
+    <div id="footer" class="row">
+      <div class="col-md-6">
+        <p class="text-muted small">Footer Stuff Goes Here, Ok La?</p>
+      </div>
+      <div class="col-md-6">
+        <p class="text-muted small text-right">And more footer stuff can be here too? Good? Eh?</p>
+      </div>
+    </div>
+  </div>
+
+    <!--Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+  </html>
